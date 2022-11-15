6ff1fa61c20742bd5da594b29a4129bc4bfa25f3
SHREYA PRASAD <prasadshreya2303@gmail.com>
2022-11-15 15:36:54 +0530

    Initial Commit

---
+++ Weather_App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather_App
+//
+//  Created by Shreya Prasad on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
